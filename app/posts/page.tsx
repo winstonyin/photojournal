8048526f4f2446603848d35e401f2848daa8e885
Winston Yin <winstonyin@gmail.com>
2023-07-22 01:57:26 -0700

    markdown-to-jsx

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,22 @@
+import Markdown from "markdown-to-jsx"
+import Photo from "../components/photo"
+
 export default async function Posts() {
-  return <h1>POSTS</h1>
+  return <Markdown
+    options={{
+      overrides: {
+        Photo: {
+          component: Photo,
+        }
+      }
+    }}
+  >
+    {`
+# header
+1. item 1
+2. item 2
+
+<Photo src="/content/albums/Even More/For Mine (sharing)-31.jpg" url="/albums/Even More"} />
+    `}
+  </Markdown>
 }
