36cdce8e3eadae18f386efea5cf8ce761419ad93
Winston Yin <winstonyin@gmail.com>
2023-08-07 20:19:28 +0800

    icons

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { usePathname } from 'next/navigation'
 import Logo from "./navbar/logo"
 import ThemeToggle from "./navbar/theme-toggle"
+import { LanguageIcon, PhotoIcon, NewspaperIcon } from "@heroicons/react/24/solid"
 
 export default function Navbar() {
   const pathname = usePathname()
@@ -19,21 +20,27 @@ export default function Navbar() {
                 href="/albums"
                 className={pathname.startsWith("/albums") ?
                   "grid h-16 place-items-center bg-gray-100 dark:bg-gray-800" :
-                  "grid h-16 place-items-center hover:bg-gray-100 dark:hover:bg-gray-800"}>冊</Link>
+                  "grid h-16 place-items-center hover:bg-gray-100 dark:hover:bg-gray-800"}>
+                <PhotoIcon className="w-7 h-7" />
+              </Link>
             </li>
             <li>
               <Link
                 href="/posts"
                 className={pathname.startsWith("/posts") ?
                   "grid h-16 place-items-center bg-gray-100 dark:bg-gray-800" :
-                  "grid h-16 place-items-center hover:bg-gray-100 dark:hover:bg-gray-800"}>篇</Link>
+                  "grid h-16 place-items-center hover:bg-gray-100 dark:hover:bg-gray-800"}>
+                <NewspaperIcon className="w-7 h-7" />
+              </Link>
             </li>
           </ul>
         </div>
         <div>
           <ul>
             <li>
-              <div className="grid h-16 place-items-center hover:bg-gray-100 dark:hover:bg-gray-800">文</div>
+              <div className="grid h-16 place-items-center hover:bg-gray-100 dark:hover:bg-gray-800">
+                <LanguageIcon className="w-7 h-7" />
+              </div>
             </li>
             <li>
               <div className="grid h-16 place-items-center hover:bg-gray-100 dark:hover:bg-gray-800"><ThemeToggle /></div>
