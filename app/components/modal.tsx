e102e07d210098656b136b3c58c8f4c53f7b32bf
Winston Yin <winstonyin@gmail.com>
2023-08-19 15:10:03 +0800

    mobile modal

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -9,8 +9,8 @@ export default function Modal({children}: {children: React.ReactNode}) {
   return (
     <div className="fixed inset-0 backdrop-blur-lg z-50 flex items-center justify-center">
       {children}
-      <div onClick={router.back} className="absolute top-2 left-2 w-10 h-10 grid content-center justify-center align-middle cursor-pointer text-gray-400 hover:text-gray-50 transition-colors">
-        <XMarkIcon className="w-8 h-8" />
+      <div onClick={router.back} className="absolute top-2 left-2 w-8 h-8 lg:w-10 lg:h-10 grid content-center justify-center align-middle cursor-pointer text-gray-400 hover:text-gray-50 transition-colors">
+        <XMarkIcon className="w-6 h-6 lg:w-8 lg:h-8" />
       </div>
     </div>
   )
