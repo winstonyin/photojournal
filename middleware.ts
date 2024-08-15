ef661e90a0a4a8bedb93345dfd89c3c8edf73d29
Winston Yin <winstonyin@gmail.com>
2024-08-15 02:12:51 -0700

    locale cookie

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ import site_config from "./site-config.json";
 
 function getLocale(request: NextRequest) {
   const accept_language = request.headers.get("accept-language") || ""
-  const languages = new Negotiator({headers: {"accept-language": accept_language}}).languages()
+  const cookie_language = request.cookies.get("custom_locale")?.value
+  const languages = new Negotiator({headers: {"accept-language": cookie_language || accept_language}}).languages()
   const default_locale = site_config.locales[0] || "en"
   return match(languages, site_config.locales, default_locale)
 }
@@ -13,18 +14,22 @@ function getLocale(request: NextRequest) {
 export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl
-  const pathnameHasLocale = site_config.locales.some(
+  const path_locale = site_config.locales.find(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   )
 
-  if (pathnameHasLocale) return
-
-  // Redirect if there is no locale
-  const locale = getLocale(request)
-  request.nextUrl.pathname = `/${locale}${pathname}`
-  // e.g. incoming request is /products
-  // The new URL is now /en-US/products
-  return NextResponse.redirect(request.nextUrl)
+  if (path_locale) {
+    const response = NextResponse.next()
+    response.cookies.set("custom_locale", path_locale)
+    return response
+  } else {
+    // Redirect if there is no locale
+    const locale = getLocale(request)
+    request.nextUrl.pathname = `/${locale}${pathname}`
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(request.nextUrl)
+  }
 }
  
 export const config = {
