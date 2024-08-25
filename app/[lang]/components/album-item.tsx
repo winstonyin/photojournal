23f43879271f0552a111505a92fbdbd4ff795c62
Winston Yin <winstonyin@gmail.com>
2024-08-24 21:39:40 -0700

    mobile album page

--- app/[lang]/components/album-item.tsx
+++ app/[lang]/components/album-item.tsx
@@ -15,8 +15,8 @@ export default function AlbumItem({url, title, cover, photo_count, album_count, 
   const album_count_text = album_count + config.t_album_count[config.locales.findIndex(l => l == lang)]
 
   return (
-    <Link href={"/" + lang + url} className="inline-block w-80 h-96 rounded-lg m-1 bg-gray-700 hover:scale-[1.02] transition-transform">
-      <div className="relative w-80 h-80 overflow-hidden rounded-t-lg justify-center align-middle">
+    <Link href={"/" + lang + url} className="inline-block w-[45vw] md:w-80 rounded-lg m-[1vw] md:m-1 pb-2 bg-gray-700 hover:scale-[1.02] transition-transform">
+      <div className="relative h-[45vw] md:h-80 b-12 overflow-hidden rounded-t-lg justify-center align-middle">
         <Image
           src={imageSize(cover, 320)}
           alt={title}
@@ -25,8 +25,8 @@ export default function AlbumItem({url, title, cover, photo_count, album_count, 
           unoptimized
         />
       </div>
-      <div className="block w-80 h-12 px-3 py-2 text-sm">
-        <h3 className="font-bold whitespace-nowrap overflow-hidden text-ellipsis">{title}</h3>
+      <div className="block h-12 px-3 py-2 text-sm overflow-hidden text-ellipsis whitespace-nowrap">
+        <h3 className="font-bold overflow-hidden text-ellipsis">{title}</h3>
         {album_count ? <span className="text-gray-300 mr-3">{album_count_text}</span> : null}<span className="text-gray-300">{photo_count_text}</span>
       </div>
     </Link>
--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({params, children}: {params: {lang: string}, 
                 </ul>
               </div>
             </Navbar>
-            <main className="mx-3 mt-auto h-screen md:ml-16">
+            <main className="mx-[3vw] mt-auto h-screen md:ml-16">
               {children}
             </main>
           </ModalWrapper>
