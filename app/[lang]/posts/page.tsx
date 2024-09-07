63b39e81596eb4480ec29354857d40c9ae31f449
Winston Yin <winstonyin@gmail.com>
2024-09-07 00:54:00 -0700

    post order by reverse date

--- app/[lang]/posts/page.tsx
+++ app/[lang]/posts/page.tsx
@@ -1,24 +1,45 @@
 import posts from "@/data/posts.json"
 import PostItem from "../components/post-item"
 import config from "@/site-config.json"
+import { url } from "inspector"
 
+// TODO: declare types
 export default function PostsPage({params}: {params: {lang: string}}) {
-  const post_items = posts.map((p, i) => {
-    const matching_post = p.posts.find(pl => pl.lang == params.lang)
-    if (matching_post) {
-      return (
-        <PostItem
-          key={i}
-          url={"/" + params.lang + "/posts/" + p.slug}
-          title={matching_post.title}
-          date={matching_post.date}
-          cover={matching_post.cover}
-          count={matching_post.count}
-          blurb={matching_post.blurb}
-        />
-      )
+  const post_items = posts.map(p => {
+    const ind = p.posts.findIndex(pp => pp.lang == params.lang)
+    if (ind >= 0) {
+      const pp = p.posts[ind]
+      return {
+        slug: p.slug,
+        url: pp.title,
+        title: pp.title,
+        date: pp.date,
+        cover: pp.cover,
+        count: pp.count,
+        blurb: pp.blurb
+      }
+    } else {
+      return {
+        slug: "",
+        url: "",
+        title: "",
+        date: "",
+        cover: "",
+        count: 0,
+        blurb: ""
+      }
     }
-  })
+  }).filter(p => p.slug !== "").sort((a, b) => parseInt(b.date) - parseInt(a.date)).map((p, i) =>
+    <PostItem
+      key={i}
+      url={"/" + params.lang + "/posts/" + p.slug}
+      title={p.title}
+      date={p.date}
+      cover={p.cover}
+      count={p.count}
+      blurb={p.blurb}
+    />
+  )
 
   return (
     <div className="flex flex-wrap pt-3 pb-16">
